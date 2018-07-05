4a762ce70aa40570df3a225dc284de86b41c2291
Makoto Shimazu <makoto.shimaz@gmail.com>
2018-07-05 22:18:07 +0900

    Add LICENSE file and the main sketch file

---
+++ DayCounter.ino
@@ -0,0 +1,29 @@
+//
+// Copyright 2018 Makoto Shimazu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+uint32_t time_start_ms;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  time_start_ms = millis();
+}
+
+void loop() {
+  uint32_t elapsed_s = (millis() - time_start_ms) / 1000;
+  Serial.println(elapsed_s);
+  delay(500);
+}
